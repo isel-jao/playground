af1969c5c13829ee034ed820e06cb691d2f15e3a
issam eljaouhary <iseljao89@gmail.com>
2025-11-19 14:05:33 +0100

    feat: implement Icon component for dynamic SVG loading

--- react-vite/vite.config.ts
+++ react-vite/vite.config.ts
@@ -1,13 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    tailwindcss(),
+    svgr(),
   ],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
