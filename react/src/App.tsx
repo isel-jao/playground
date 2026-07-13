fabb81b6a281ac4dc060bc069928d5c6503f3029
issam el jaouhary <iseljao89@gmail.com>
2026-07-13 14:43:06 +0100

    feat: implement Button and Card components with Tailwind CSS styling

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,3 +1,12 @@
+import { Button } from "./components/button";
+import { Card } from "./components/card";
+
 export default function App() {
-return <main className="container">App</main>;
+  return (
+    <main className="container ">
+      <Card>
+        <Button>click me</Button>
+      </Card>
+    </main>
+  );
 }
